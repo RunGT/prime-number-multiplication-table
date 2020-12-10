703ea313e3791ccb6351c833ddceb39289f5e5a4
Glenroy-Terence (RunGT) <glenroyterencedev@gmail.com>
2020-12-10 14:03:59 +0000

    Test N is at least one

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-// userInputNumber(2)
-
 const userInputNumber = (n) => {
-    if ( typeof(n) !== "number") {
- return "Value entered is not a number"
-    }
- return n;
+  if (typeof n !== "number") {
+    return "Value entered is not a number";
+  }
+  if (n % 1 !== 0) {
+    return "Whole number is required";
+  }
+  if (n < 1 || 0){
+      return "A minimum value of 1 is required"
+  }
+  return n;
 };
 
-module.exports =userInputNumber;
+module.exports = userInputNumber;
--- test/prime-multiplication.test.js
+++ test/prime-multiplication.test.js
@@ -1,12 +1,20 @@
 const { test, expect } = require("@jest/globals");
-const { describe } = require("yargs");
 const userInputNumber = require("../main");
 
-
+describe("Check N meets requirements", () => {
     test("N is a number", () => {
         expect(userInputNumber(3)).toBe(3);
         expect(userInputNumber("Three")).toBe("Value entered is not a number");
     });
-    
+
+    test("N is a whole number", () => {
+        expect(userInputNumber(2.5)).toBe("Whole number is required")
+    });
+
+    test("Value is at least 1", () => {
+        expect(userInputNumber(0)).toBe("A minimum value of 1 is required")
+        expect(userInputNumber(-1)).toBe("A minimum value of 1 is required")
+    });
+});
 
 
