b8b15c603e068b6d13b1363a786a2a5d31a68de2
Glenroy-Terence (RunGT) <glenroyterencedev@gmail.com>
2020-12-10 13:44:22 +0000

    Test for string value user edge case

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 // userInputNumber(2)
 
 const userInputNumber = (n) => {
- return n
-
+    if ( typeof(n) !== "number") {
+ return "Value entered is not a number"
+    }
+ return n;
 };
 
 module.exports =userInputNumber;
--- test/prime-multiplication.test.js
+++ test/prime-multiplication.test.js
@@ -1,12 +1,12 @@
-// Is is it a whole number?
-// Is it at least 1?
-
-const { test } = require("@jest/globals");
+const { test, expect } = require("@jest/globals");
 const { describe } = require("yargs");
 const userInputNumber = require("../main");
 
 
     test("N is a number", () => {
-        expect(userInputNumber(2)).toBe(2);
+        expect(userInputNumber(3)).toBe(3);
+        expect(userInputNumber("Three")).toBe("Value entered is not a number");
     });
+    
+
 
