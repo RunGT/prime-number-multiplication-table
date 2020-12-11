ddb1eef2594a57f15a32e7ae4689f9f97331c66d
Glenroy-Terence (RunGT) <glenroyterencedev@gmail.com>
2020-12-11 15:49:07 +0000

    Edit out dated test and change test to check for a correct multiplication table

--- test/createPrimes.test.js
+++ test/createPrimes.test.js
@@ -2,12 +2,16 @@ const { test, expect } = require("@jest/globals");
 const userInputNumber = require("../main");
 
 test("Create an array from N", () => {
- const expected = [2]
+  const expected = [2];
   expect(userInputNumber(1)).toStrictEqual(expected);
 });
 
-const expected = [2,3,5]
-test("Check numbers in the array are prime", () => {
- expect(userInputNumber(3)).toStrictEqual(expected)
-
-})
+const expected = [
+  [null, 2, 3, 5],
+  [2, 4, 6, 10],
+  [3, 6, 9, 15],
+  [5, 10, 15, 25],
+];
+test("Check prime multiplication table is correct", () => {
+  expect(userInputNumber(3)).toStrictEqual(expected);
+});
