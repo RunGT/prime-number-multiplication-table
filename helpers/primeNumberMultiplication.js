80fe260516ea4c75750839301afcb401d7e63e51
Glenroy-Terence (RunGT) <glenroyterencedev@gmail.com>
2020-12-14 11:18:13 +0000

    Add comments

--- helpers/primeNumberMultiplication.js
+++ helpers/primeNumberMultiplication.js
@@ -1,10 +1,13 @@
 module.exports = (numArr) => {
   
+  // firstRow is the first row with null and adds the arrary using the spread operator
   let firstRow = [null, ...numArr];
   let allRowsColumns = [firstRow];
 
   numArr.map((rowOfPrimes) => {
     let multiples = [rowOfPrimes];
+
+
     numArr.map((columnOfPrimes) => {
       let multiplication = rowOfPrimes * columnOfPrimes;
 
--- helpers/primeNumbers.js
+++ helpers/primeNumbers.js
@@ -1,3 +1,5 @@
+// A function to check if n is a prime number
+
 const isPrime = (n) => {
   for (let i = 2; i < n; i++) if (n % i === 0) return false;
   return n > 1;
